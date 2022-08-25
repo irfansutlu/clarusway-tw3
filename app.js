3f4b89701ab1f1df869ba355676d86ffd29e2155
İrfan Sütlü <peroxcid@haotmail.com>
2022-08-25 11:52:48 +0300

    clarusway-tw3

---
+++ app.js
@@ -0,0 +1,11 @@
+// const reString=(str) => str.split('').reverse().join('');
+
+// let fString= prompt('Terse çevrilmesini istediğiniz metni girin...')
+// alert(reString(fString));
+
+//---------------------------------
+
+// const reString=(str) => str.split(' ').reverse().join(' ');
+
+// let fString= prompt('Terse çevrilmesini istediğiniz metni girin...')
+// alert(reString(fString));
